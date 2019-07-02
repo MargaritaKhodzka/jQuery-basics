f5606453075e4f5531b014b048190786a438eee7
MargaritaKhodzka <mkhodzka@gmail.com>
2019-07-01 23:08:51 -0400

    add button to the DOM with jQuery

--- 2_spoiler/js/app.js
+++ 2_spoiler/js/app.js
@@ -1,8 +1,17 @@
+// create "Reveal Spoiler" button
+const spoilerButton = $('<button>Reveal Spoiler</button>')
+// append button to the page
+$('.spoiler').append(spoilerButton);
+
 // hide the spoiler on page load
 const spoilerText = $('.spoiler span').hide();
-const spoilerButton = $('.spoiler button');
 
-// when the button is pressed - show the spoiler text and hide the button
+/*
+when the button is pressed:
+ - show the spoiler text
+ - hide the button
+*/
+
 spoilerButton.click( () => {
   spoilerText.show();
   spoilerButton.hide();
