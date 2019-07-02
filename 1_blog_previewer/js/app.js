542011945a0db1dd5774ecd3741eec99d2b1c785
MargaritaKhodzka <mkhodzka@gmail.com>
2019-07-01 22:30:52 -0400

    changing content inside elements

--- 1_blog_previewer/js/app.js
+++ 1_blog_previewer/js/app.js
@@ -2,3 +2,9 @@ $('#flashMessage').hide()
   .slideDown(1000)
   .delay(3000)
   .slideUp();
+
+const title = 'My first blog post';
+const content = 'This is my <strong>first</strong> post!';
+
+$('#blogTitlePreview').text(title);
+$('#blogContentPreview').html(content);
