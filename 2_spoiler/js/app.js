90d37e44b42d19da731b0f655d091952adb3a015
MargaritaKhodzka <mkhodzka@gmail.com>
2019-07-01 22:57:00 -0400

    spoiler button click event to show spoiler text

--- 2_spoiler/js/app.js
+++ 2_spoiler/js/app.js
@@ -1,5 +1,9 @@
+// hide the spoiler on page load
+const spoilerText = $('.spoiler span').hide();
+const spoilerButton = $('.spoiler button');
 
-
-
-
-
+// when the button is pressed - show the spoiler text and hide the button
+spoilerButton.click( () => {
+  spoilerText.show();
+  spoilerButton.hide();
+});
